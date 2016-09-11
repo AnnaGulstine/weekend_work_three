b4a51e18b5f29cb21abb183b13de617f4ab638dd
verynear <scott.a.near@gmail.com>
2016-09-10 23:42:15 -0500

    solved oop hotel_reservation

--- oop/hotel_reservation.rb
+++ oop/hotel_reservation.rb
@@ -32,3 +32,42 @@ end
 # The add_a_crib method
 # The add_a_custom_amenity method
 
+reservation1 = HotelReservation.new(customer_name: "John Booth", date: "July 3rd", room_number: 613)
+
+p reservation1
+
+reservation1.room_number = 415
+
+reservation1.add_a_fridge
+
+reservation1.add_a_crib
+
+reservation1.add_a_custom_amenity("microwave")
+
+if reservation1.room_number == 415 && reservation1.amenities.include?("fridge") && reservation1.amenities.include?("crib") && reservation1.amenities.include?("microwave")
+  result = "PASS"
+else
+  result = "F"
+end
+
+puts result
+
+reservation2 = HotelReservation.new(customer_name: "Peter Pan", date: "January 15th", room_number: 708)
+
+p reservation2
+
+reservation2.room_number = 316
+
+reservation2.add_a_fridge
+
+reservation2.add_a_crib
+
+reservation2.add_a_custom_amenity("microwave")
+
+if reservation2.room_number == 316 && reservation2.amenities.include?("fridge") && reservation2.amenities.include?("crib") && reservation2.amenities.include?("microwave")
+  result = "PASS"
+else
+  result = "F"
+end
+
+puts result
